e2017dd939b6eea633427981727699bd48e0130f
ykimdeveloper <ykimdeveloper@gmail.com>
2025-10-22 21:32:50 -0700

    Update PlatformJS.jsx

--- src/views/PlatformJS.jsx
+++ src/views/PlatformJS.jsx
@@ -73,7 +73,7 @@ export default function PlatformView() {
         {browser === "Opera" ? (
         <BrowserWarning browser={browser} device={device} />
         ) : (
-            <DivWithSize label={`Dynamic: {current.label} — Fails on Opera Browser`} debug>
+            <DivWithSize label={`Dynamic: ${current.label} — Fails on Opera Browser`} debug >
             <div style={current.style}>
             <MyMap />
             </div>
